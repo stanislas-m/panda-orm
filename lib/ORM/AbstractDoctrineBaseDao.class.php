b768d7293be7ca930b0820cf989ee3e99e7ad923
Stanislas Michalak <stanislas.michalak@gmail.com>
2014-11-10 00:05:25 +0100

    First files.

---
+++ lib/ORM/AbstractDoctrineBaseDao.class.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Panda\ORM;
+
+use Panda\Core\Component\Bundle\Dao\AbstractBasicDao;
+
+class AbstractDoctrineBaseDao extends AbstractBasicDao
+{
+
+} 
---
+++ lib/ORM/AbstractSpotBaseDao.class.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Panda\ORM;
+
+use Panda\Core\Component\Bundle\Dao\AbstractBasicDao;
+
+class AbstractSpotBaseDao extends AbstractBasicDao
+{
+
+}
