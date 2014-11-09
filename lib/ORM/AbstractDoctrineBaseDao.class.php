13e4fbb952ee7764fab4183717fa95f15cf5983c
Stanislas Michalak <stanislas.michalak@gmail.com>
2014-11-10 00:26:07 +0100

    Spot: getEntityManager

--- lib/ORM/AbstractDoctrineBaseDao.class.php
+++ lib/ORM/AbstractDoctrineBaseDao.class.php
@@ -6,5 +6,10 @@ use Panda\Core\Component\Bundle\Dao\AbstractBasicDao;
 
 class AbstractDoctrineBaseDao extends AbstractBasicDao
 {
+    protected static $entityManagers = array();
 
+    public function getEntityManager($entityManagerName)
+    {
+
+    }
 } 
--- lib/ORM/AbstractSpotBaseDao.class.php
+++ lib/ORM/AbstractSpotBaseDao.class.php
@@ -3,8 +3,41 @@
 namespace Panda\ORM;
 
 use Panda\Core\Component\Bundle\Dao\AbstractBasicDao;
+use Panda\Core\Component\Config\ConfigManager;
+use Spot\Config;
+use Spot\Locator;
 
 class AbstractSpotBaseDao extends AbstractBasicDao
 {
+    protected static $config;
+    protected static $locator;
 
+    public function __construct()
+    {
+        if (self::$config === null) {
+            self::$config = new Config();
+        }
+    }
+
+    public function getEntityManager($entityManagerName)
+    {
+        if ($entityManagerName === null) {
+            $entityManagerName = ConfigManager::get('database.default');
+        }
+
+        if (!array_key_exists(self::$connections, $entityManagerName)) {
+
+            $connectionParams = ConfigManager::get('database.list.' . $entityManagerName);
+
+            self::$config->addConnection($entityManagerName, $connectionParams);
+
+            if (self::$locator === null) {
+                self::$locator = new Locator(self::$config);
+            } else {
+                self::$locator->config(self::$config);
+            }
+        }
+
+        return self::$locator;
+    }
 }
